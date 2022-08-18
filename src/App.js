5a5479999e054822b9c7f368dfdcbd9933d5b9cb
dora kojevnikov <feodora.k@gmail.com>
2022-08-18 17:27:51 +1000

    login route ready

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route, NavLink, useNavigate } from 'react-router-dom'
-import React, { Component }  from 'react';
+import React, { useState, useEffect, Component }  from 'react';
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -8,17 +8,38 @@ import './App.css';
 import Home from "./components/Home";
 import NavBar from './components/NavBar';
 import Map from './components/Map';
+import Login from './components/Login'
+import Register from './components/Register'
+import Logout from './components/Logout'
 
-const location = {
-  address: 'Uluru',
-  lat: -25.363,
-  lng: 131.044,
-}
+
 
 
 const App = () =>   {
+  const [authorised, setAuthorised] = useState(null)
 
   const navigate = useNavigate()
+
+  const location = {
+    address: 'Uluru',
+    lat: -25.363,
+    lng: 131.044,
+  }
+  
+  const handleAuth = (authed) => {
+    setAuthorised(authed)
+    navigate("/")
+  }
+  
+  useEffect(() => {
+    const checkIfLoggedIn = async () => {
+      const res = await fetch("/users/isauthorised")
+      const data = await res.json()
+      console.log(data.msg)
+      setAuthorised(data.authorised)
+    }
+    checkIfLoggedIn()
+  }, [])
 
   return (
     <div className="App">
@@ -27,12 +48,20 @@ const App = () =>   {
       </span>
       <header className="App-header">
         <Routes>
+
             <Route path="/" element={
-            <>
-            <Home />
-            <Map location={location} zoomLevel={12} />
-            </>
+              <>
+                <Home />
+                <Map location={location} zoomLevel={12} />
+              </>
             }/>
+
+            <Route path="/login" element={
+              <>
+                <Login handleLogin={handleAuth} />
+              </>
+            } />
+
         </Routes>
       </header>
     </div>
---
+++ src/components/Login.jsx
@@ -0,0 +1,62 @@
+import React, { useState, Component } from 'react'
+import { Link } from 'react-router-dom'
+
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+
+const Login = (props) => {
+  const [fields, setFields] = useState({ username: '', password: '' })
+
+  const handleChange = (event) => { 
+    setFields({
+      ...fields,
+      [event.target.name]: event.target.value
+    })
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    const res = await fetch("/users/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json"},
+      body: JSON.stringify(fields)
+    })
+    const data = await res.json()
+    console.log(data.msg)
+    props.handleLogin(data.authorised)
+  }
+
+  return (
+    <div className="container register">
+      <Form onSubmit={handleSubmit}>
+        <h1>Login</h1>
+        <Form.Group>
+          <Form.Label htmlFor="username">Username</Form.Label>
+          <Form.Control
+            value={fields.username}
+            onChange={handleChange}
+            name="username"
+            type="text"
+            id="username"
+            className="form-text"
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label htmlFor="password">Password</Form.Label>
+          <Form.Control
+            value={fields.password}
+            onChange={handleChange}
+            name="password"
+            type="password"
+            id="password"
+            className="form-text"
+          />
+        </Form.Group><br/>
+      <Button type="submit" value="Login" variant="light">Login</Button>
+      <p>No account yet? <Link to="/register" className="link-text">Register here</Link></p>
+    </Form>
+    </div>
+  )
+}
+
+export default Login
