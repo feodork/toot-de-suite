4dc18cb3ef9ddf94fd7677b9ce3de884df81c717
dora kojevnikov <feodora.k@gmail.com>
2022-08-23 08:23:31 +1000

    popups added

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,22 +1,31 @@
 import React, { useState, useEffect } from "react";
-import { MapContainer, TileLayer, useMap, Marker } from 'react-leaflet'
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 
 import toiletData from "../data/toilet-data.json"
 import "leaflet/dist/leaflet.css"
 
 const Map = () => {
 
+const filteredToilets = toiletData.filter(toots => toots.State === "NT")
+
   return (
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+      <MapContainer center={[-12.4637, 130.8444]} zoom={6} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {toiletData.map(toot => (
+        {filteredToilets.map(toot => (
           <Marker 
-          key = {toot._id}
+          key={toot._id}
           position={[toot.Latitude, toot.Longitude]}>
-
+            <Popup position={[toot.Latitude, toot.Longitude]}>
+              <div>
+                <h3>{toot.Name}</h3>
+                <p>{toot.Address1}, {toot.Town} {toot.State}</p>
+                <p>Men: {toot.Male} Women: {toot.Female} Unisex {toot.Unisex} All Gender: {toot.AllGender}</p>
+                <p>Accessible: {toot.Accessible}</p>
+              </div>
+            </Popup>
           </Marker>
         ))}
 
